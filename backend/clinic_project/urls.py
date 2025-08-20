cc65a2d26b1fc49808409a1728b7ab1d9c8e4fde
Eduardo Basso <edu_fabric@outlook.com>
2025-08-20 12:04:16 -0300

    feat: redirect root to admin UI

--- backend/clinic_project/urls.py
+++ backend/clinic_project/urls.py
@@ -2,10 +2,13 @@
 # backend\clinic_project\urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from apps.register.authentication import EmailTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    # Redirect root to the admin UI to make the admin entry point easy to find
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('admin/', admin.site.urls),
     path('register/', include('apps.register.urls')),  # 🧩 Rotas do app clínico
 
