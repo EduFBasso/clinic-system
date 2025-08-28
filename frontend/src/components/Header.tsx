387ee58455342e6c9affaa62340396a3256fbc4d
Eduardo Basso <edu_fabric@outlook.com>
2025-08-28 06:36:56 -0300

    testes manuais com adição de clientes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/components/Header.module.css';
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h1 className={styles.headerTitle}>Clinic System</h1>
+            <h1 className={styles.headerTitle}>Sistema Clinico</h1>
         </header>
     );
 };
