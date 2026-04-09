a38531cc44a97620fcc2c9d71206e7c2076825ec
Eduardo Figueiredo Basso <edu_fabric@outlook.com>
2026-04-09 17:17:46 -0300

    fix: disable SECURE_SSL_REDIRECT in CI environment

--- backend/clinic_project/settings.py
+++ backend/clinic_project/settings.py
@@ -204,7 +204,7 @@ EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 
-if not DEBUG:
+if not DEBUG and not _IN_CI:
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
